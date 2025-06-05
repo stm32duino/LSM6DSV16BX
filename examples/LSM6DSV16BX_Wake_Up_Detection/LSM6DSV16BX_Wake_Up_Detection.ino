18096345c506c43871afb92ae379f477880093cb
Carlo PARATA <carlo.parata@st.com>
2025-06-05 16:32:26 +0200

    Fix compilation issue

--- examples/LSM6DSV16BX_Wake_Up_Detection/LSM6DSV16BX_Wake_Up_Detection.ino
+++ examples/LSM6DSV16BX_Wake_Up_Detection/LSM6DSV16BX_Wake_Up_Detection.ino
@@ -1,10 +1,10 @@
 // Tested with X-NUCLEO-IKS01A3
-#include <LSM6DSV16XSensor.h>
+#include <LSM6DSV16BXSensor.h>
 
 #define INT_1 A5
 
 //I2C_ADD_L for compatibility with X-NUCLEO-IKS01A3
-LSM6DSV16XSensor LSM6DSV16X(&Wire, LSM6DSV16BX_I2C_ADD_L);
+LSM6DSV16BXSensor LSM6DSV16BX(&Wire, LSM6DSV16BX_I2C_ADD_L);
 
 //Interrupts.
 volatile int mems_event = 0;
@@ -27,19 +27,19 @@ void setup()
   attachInterrupt(INT_1, INT1Event_cb, RISING);
 
   // Initlialize components.
-  LSM6DSV16X.begin();
-  LSM6DSV16X.Enable_X();
+  LSM6DSV16BX.begin();
+  LSM6DSV16BX.Enable_X();
 
   // Enable Wake Up Detection.
-  LSM6DSV16X.Enable_Wake_Up_Detection(LSM6DSV16X_INT1_PIN);
+  LSM6DSV16BX.Enable_Wake_Up_Detection(LSM6DSV16BX_INT1_PIN);
 }
 
 void loop()
 {
   if (mems_event) {
     mems_event = 0;
-    LSM6DSV16X_Event_Status_t status;
-    LSM6DSV16X.Get_X_Event_Status(&status);
+    LSM6DSV16BX_Event_Status_t status;
+    LSM6DSV16BX.Get_X_Event_Status(&status);
     if (status.WakeUpStatus) {
       // Led blinking.
       digitalWrite(LED_BUILTIN, HIGH);
